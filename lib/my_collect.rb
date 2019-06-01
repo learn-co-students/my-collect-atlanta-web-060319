70cea4332ba72e4b4792ec0cf1bd08b128d55390
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-06-01 00:06:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+  i = 0
+  result = []
+  while i < array.length
+    result.push(yield array[i])
+      i += 1
+  end
+  result
+end
+
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
